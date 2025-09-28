005ad4aaab88a469cea867bf0275d7a6561d860d
fauzanfebrian <fauzanfebriann@gmail.com>
2025-09-28 09:27:06 +0700

    bsd_compat: Declare setmode and getmode prototypes

--- bsd_compat/include/bsd_compat.h
+++ bsd_compat/include/bsd_compat.h
@@ -1,5 +1,11 @@
+#include <sys/types.h>
+
 #define __dead __attribute__((__noreturn__))
 
 const char *getprogname(void);
 
 extern int optreset;
+
+void *setmode(const char *);
+
+mode_t getmode(const void *bbox, mode_t omode);
